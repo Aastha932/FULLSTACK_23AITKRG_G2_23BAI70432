ba0b0a643f95b5f1a19a0cdd9032d8b57634df0c
Aastha932 <aastha.j932006@gmail.com>
2026-03-12 14:57:35 +0530

    Add Header component with navigation links

---
+++ EXP2/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header style = {{
+        padding: '10px',
+        backgroundColor: "#000000FF",
+        color : 'white',
+        textAlign: 'center',
+      }}>
+      <h1>EcoTrack-2</h1>
+      <Link style={{paddingRight: "10px", color: "#00fff0"}} to = "/">Dashboard</Link>
+      <Link style={{padding: "10px", color: "#00fff0"}} to = "/logs">Logs</Link>
+      <Link style={{padding: "10px", color: "#00fff0"}} to = "/login">Login</Link>
+    </header>
+  )
+}
+
+export default Header;
