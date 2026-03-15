39d52b41c4aa9aeef6f134e0969c36baf848937c
Aastha932 <aastha.j932006@gmail.com>
2026-03-15 23:18:58 +0530

    Add DTO, Entity, Repository, and Service files to exp-6

---
+++ exp-6/src/main/java/com/example/demo/dto/AppointmentDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import java.time.LocalDate;
+
+public class AppointmentDTO {
+
+    public Long id;
+    public LocalDate appointmentDate;
+    public Long patientId;
+    public Long doctorId;
+}
+avatar
+Ask In Chat
+Ask In Chat
